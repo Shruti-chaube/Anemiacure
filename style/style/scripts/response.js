fe799b2f3ed9d6a4a2ef9f4b050ad5970a583852
shruti <shrutichaube27@gmail.com>
2021-07-25 14:27:58 +0530

    shruti

--- style/style/scripts/response.js
+++ style/style/scripts/response.js
@@ -1,15 +1,15 @@
 function getBotResponse(input){
-    if(input == "hello"){
+    if(input == "Hello"){
         return "Hello there!";
-    }else if(input=="bye"){
+    }else if(input=="Bye"){
         return "Bye, Talk to you later!"
     }
    
-else if(input=="what is your name"){
+else if(input=="What is your name"){
     return "my name is shruti!";
-}else if(input=="hey"){
+}else if(input=="Hey"){
     return " Do you have any doubt"
-}else if(input=="no"){
+}else if(input=="No"){
     return "ok";
 }
 else if(input=="What can I do to control my anemia?"){
