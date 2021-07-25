8f217de442a873af5f73055543bd1eb6f8316414
shruti <shrutichaube27@gmail.com>
2021-07-25 14:22:25 +0530

    yes

---
+++ style/style/scripts/response.js
@@ -0,0 +1,21 @@
+function getBotResponse(input){
+    if(input == "hello"){
+        return "Hello there!";
+    }else if(input=="bye"){
+        return "Bye, Talk to you later!"
+    }
+   
+else if(input=="what is your name"){
+    return "my name is shruti!";
+}else if(input=="hey"){
+    return " Do you have any doubt"
+}else if(input=="no"){
+    return "ok";
+}
+else if(input=="What can I do to control my anemia?"){
+    return "Taking iron supplement pills and getting enough iron in your food will correct most cases of iron deficiency anemia.";
+}else{
+return "Goodbye";
+}
+
+}
